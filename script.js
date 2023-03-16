650d73c08bde73c61c52364b39bfaf6f823399f8
Chethiya2001 <chethiyabandaraherath@gmail.com>
2023-03-16 15:25:17 +0530

    Hospital system

---
+++ script.js
@@ -0,0 +1,9 @@
+//get element from the DOM //
+
+const menu =document.querySelector('.menu');
+const menuBtn =document.querySelector('.menu-btn');
+
+//toggle open/close menu //
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('menu-open');
+});
